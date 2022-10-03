9751f6f8d722009930e6fa33db306a91bf9e519f
kangcodeis <miiren543@damu.net>
2022-10-03 18:41:50 +0900

    Updates

---
+++ precache-manifest.deaf4ef7af59ed4d9b7e789a8c7e3d65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be45c4c77394c99fd688dc54fa648922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a39386d34e5ad103b5ce",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f686bcf2b93236aa1a39",
+    "url": "/static/css/main.d10d0330.chunk.css"
+  },
+  {
+    "revision": "a39386d34e5ad103b5ce",
+    "url": "/static/js/2.4299d6f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4299d6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f686bcf2b93236aa1a39",
+    "url": "/static/js/main.9868ea07.chunk.js"
+  },
+  {
+    "revision": "9f0e2f2630ca9dcd4f57",
+    "url": "/static/js/runtime-main.8c109fa6.js"
+  },
+  {
+    "revision": "d22363c27eebaf507055e262e51012c0",
+    "url": "/static/media/curved.d22363c2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kangcodeis.github.io/precache-manifest.63b8fcb9e6b9d11ebf5811722e5fee67.js"
+  "/precache-manifest.deaf4ef7af59ed4d9b7e789a8c7e3d65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kangcodeis.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
